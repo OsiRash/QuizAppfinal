66f486544b121039e6d04292eefe505504dbe989
osama <os.rasheed@gmail.com>
2018-02-01 00:26:18 +0100

    new update

--- app/src/main/java/com/example/oaamarasheed/quizapp/PopUpRightanswers.java
+++ app/src/main/java/com/example/oaamarasheed/quizapp/PopUpRightanswers.java
@@ -1,8 +1,15 @@
 package com.example.oaamarasheed.quizapp;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+
+import android.view.View;
 import android.widget.TextView;
+
+
 
 /**
  * Created by oaamarasheed on 15/01/18.
@@ -12,6 +19,18 @@ public class PopUpRightanswers extends Activity {
 
  TextView niceJop;
 
+    public static Intent getOpenFacebookIntent(Context context) {
+
+        try {
+            context.getPackageManager()
+                    .getPackageInfo("com.facebook.katana", 0); //Checks if FB is even installed.
+            return new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("fb://profile")); //Trys to make intent with FB's URI
+        } catch (Exception e) {
+            return new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://www.facebook.com/arkverse")); //catches and opens a url to the desired page
+        }
+    }
 
 
 
@@ -22,6 +41,7 @@ public class PopUpRightanswers extends Activity {
 
 
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.popupright);
 //this changes the widht and hight of the pupmassege
         DisplayMetrics imageSize = new DisplayMetrics();
@@ -35,10 +55,10 @@ niceJop = (TextView) findViewById(R.id.nice_job);
 
 
 
-
-
-
-
+    }
+    public void onCLick(View view){
+        Intent facebookIntent = getOpenFacebookIntent(this);
+        startActivity(facebookIntent);
 
     }
 
